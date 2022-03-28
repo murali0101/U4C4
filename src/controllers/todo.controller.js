b8cfed159adde89b63b72d3d7e256554058fc63b
murali0101 <muralimv0101@gmail.com>
2022-03-28 10:54:06 +0530

    third push

---
+++ src/controllers/todo.controller.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const authenticate = require("../middleware/authenticate");
+const router = express.Router();
+const User = require("../models/user.model");
+const Todo = require("../models/todo.model");
+router.post("/", authenticate, async (req, res) => {
+  try {
+    req.body.user_id = req.user;
+    let todo = await Todo.create(req.body);
+    return res.status(200).send(todo);
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+});
+router.get("/", authenticate, async (req, res) => {
+  try {
+    req.body.user_id = req.user;
+    let todo = await Todo.find({ user_id: req.body.user_id }).lean().exec();
+    return res.status(200).send(todo);
+  } catch (error) {
+    return res.status(401).send(error.message,"Please login");
+  }
+});
+router.get("/:id", authenticate, async (req, res) => {
+  try {
+    req.body.user_id = req.user;
+    let todo = await Todo.find(req.params.id);
+    return res.status(200).send(todo);
+  } catch (error) {
+    return res.status(401).send(error.message,"Please login");
+  }
+});
+router.get("/:id", authenticate, async (req, res) => {
+  try {
+    req.body.user_id = req.user;
+    let todo = await Todo.find(req.params.id);
+    return res.status(200).send(todo);
+  } catch (error) {
+    return res.status(401).send(error.message,"Please login");
+  }
+});
+
+module.exports = router;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+const { use } = require("bcrypt/promises");
 const express = require("express");
 const app = express();
+const  { register, login } = require("./controllers/auth.controller")
+const todoController=require("./controllers/todo.controller")
+app.use(express.json())
+
+
+
+app.post("/register", register);
+app.post("/login", login);
+app.use("/todos", todoController);
 module.exports = app;
